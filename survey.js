a9264aed02995423d4a2f4f72e69885df33adb1d
jessonziegler <jziegleraudio@gmail.com>
2021-09-26 07:28:33 +0000

    survey app

---
+++ survey.js
@@ -0,0 +1,30 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What is your name? ', (answer1) => {
+  rl.question("What's an activity you like doing? ", (answer2) => {
+    rl.question("What do you listen to while doing that? ", (answer3) => {
+      rl.question("Which meal is your favourite? ", (answer4) => {
+        rl.question("What's your favourite thing to eat for that meal? ", (answer5) => {
+          rl.question("Which sport is your absolute favourite? ", (answer6) => {
+            rl.question("What is your superpower? In a few words, tell us what you are amazing at! ", (answer7) => {
+                console.log(`${answer1} enjoys ${answer2} while listening to ${answer3} during ${answer4} he always eats ${answer5} while watching ${answer6} and he knows how to ${answer7}  ` )
+                rl.close();
+
+            });
+          });
+        });
+      });
+    });
+  });
+});
+
+
+
+
+//Devani loves listening to Ludovico Einaudi while coding, devouring Yak Momos for brunch, prefers Tennis over any other sport, and is amazing at dropping mad puns at inopportune times.
+
